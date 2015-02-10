ba5b6f040636233919863778d3956b05e82d68cc
John Spray <john.spray@redhat.com>
2015-02-10 14:16:34 +0000

    mds: make purge op limit dynamic based on PGs/MDSs
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/mds/MDCache.h
+++ src/mds/MDCache.h
@@ -1068,6 +1068,7 @@ public:
   void process_delayed_expire(CDir *dir);
   void discard_delayed_expire(CDir *dir);
 
+  void notify_mdsmap_changed();
 
 protected:
   void dump_cache(const char *fn, Formatter *f);
--- src/mds/MDS.cc
+++ src/mds/MDS.cc
@@ -1796,6 +1796,8 @@ void MDS::handle_mds_map(MMDSMap *m)
     set_osd_epoch_barrier(osd_epoch);
   }
 
+  mdcache->notify_mdsmap_changed();
+
  out:
   beacon.notify_mdsmap(mdsmap);
 
